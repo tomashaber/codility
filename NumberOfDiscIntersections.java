7801a76cc1c60de9c0ea89fd912b8be4de58cbf1
tomashaber <tomas.haber@gmail.com>
2020-11-18 22:26:58 +0100

    Update NumberOfDiscIntersections.java

--- NumberOfDiscIntersections.java
+++ NumberOfDiscIntersections.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
  * @since 8.4.2015
  */
 public class NumberOfDiscIntersections {
-    public int solution(int[] A) {
-        int[][] events = new int[A.length*2][2];
+       public int solution(int[] A) {
+        long[][] events = new long[A.length*2][2];
         for (int i = 0; i < A.length; i++) {
-            events[2*i] = new int[]{i - A[i], +1};
-            events[2*i + 1] = new int[]{i + A[i], -1};
+            events[2*i] = new long[]{(long)i - A[i], +1};
+            events[2*i + 1] = new long[]{(long)i + A[i], -1};
         }
-        Comparator<int[]> comparing0 = comparing(event -> event[0]);
-        Comparator<int[]> comparing1 = Comparator.comparing(event -> -1 * event[1]);
+        Comparator<long[]> comparing0 = comparing(event -> event[0]);
+        Comparator<long[]> comparing1 = Comparator.comparing(event -> -1 * event[1]);
         Arrays.sort(events, comparing0.thenComparing(comparing1));
 
         int intersections = 0;
@@ -22,10 +22,8 @@ public class NumberOfDiscIntersections {
                 intersections += activeCircles;
                 activeCircles++;
             } else {
-
                 activeCircles--;
             }
-
         }
         if (intersections >= 10e6) {
             return -1;
