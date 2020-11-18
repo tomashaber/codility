011f72025c08c71fd4fe14a08df81e8ec3ea5210
tomashaber <tomas.haber@gmail.com>
2020-11-18 22:42:07 +0100

    Update NumberOfDiscIntersections.java

--- NumberOfDiscIntersections.java
+++ NumberOfDiscIntersections.java
@@ -5,30 +5,36 @@ import java.util.Arrays;
  * @since 8.4.2015
  */
 public class NumberOfDiscIntersections {
-       public int solution(int[] A) {
-        long[][] events = new long[A.length*2][2];
-        for (int i = 0; i < A.length; i++) {
-            events[2*i] = new long[]{(long)i - A[i], +1};
-            events[2*i + 1] = new long[]{(long)i + A[i], -1};
-        }
-        Comparator<long[]> comparing0 = comparing(event -> event[0]);
-        Comparator<long[]> comparing1 = Comparator.comparing(event -> -1 * event[1]);
-        Arrays.sort(events, comparing0.thenComparing(comparing1));
+    public int solution(int[] A) {
 
-        int intersections = 0;
-        int activeCircles = 0;
-        for (int i = 0; i < events.length; i++) {
-            if (events[i][1] == 1) {
-                intersections += activeCircles;
-                activeCircles++;
-            } else {
-                activeCircles--;
-            }
+        long intersections = 0;
+        long[] upper = new long[A.length];
+        long[] lower = new long[A.length];
+
+
+        for (int i=0;i<A.length;i++){
+            upper[i] = i + A[i];
+            lower[i] = i - A[i];
         }
-        if (intersections >= 10e6) {
+
+        Arrays.sort(upper);
+        Arrays.sort(lower);
+
+        int lower_index = 0;
+
+        for (int i=0;i<upper.length;i++)
+        {
+            while ( lower_index < A.length && upper[i]>=lower[lower_index] )
+            {
+                lower_index++;
+            }
+
+            intersections+= lower_index - i - 1;
+        }
+        if (intersections > 10000000){
             return -1;
         }
 
-        return intersections;
+        return (int)intersections;
     }
 }
